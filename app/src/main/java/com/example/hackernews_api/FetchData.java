73341ff82332e73cb206b461991f9b5fa40c1fa1
Melih Sancak <melihsancakbilkent@gmail.com>
2019-05-03 15:04:43 +0300

    Added test button and get action show action (Not in main method), it uses AsynchTask

---
+++ app/src/main/java/com/example/hackernews_api/FetchData.java
@@ -0,0 +1,21 @@
+package com.example.hackernews_api;
+
+import android.os.AsyncTask;
+
+public class FetchData extends AsyncTask<Void, Void, Void> {
+
+
+
+    @Override
+    public Void doInBackground(Void ...voids)
+    {
+        return null;
+
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+
+}
