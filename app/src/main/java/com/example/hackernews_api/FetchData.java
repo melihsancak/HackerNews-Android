f5c192e47e733fd67f570e4abe2e129a62f0050b
Melih Sancak <melihsancakbilkent@gmail.com>
2019-05-04 00:44:37 +0300

    Added layout and showing list of Json items which are not clickable

--- app/src/main/java/com/example/hackernews_api/FetchData.java
+++ app/src/main/java/com/example/hackernews_api/FetchData.java
@@ -1,21 +1,105 @@
 package com.example.hackernews_api;
 
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.util.Log;
 
-public class FetchData extends AsyncTask<Void, Void, Void> {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FetchData extends AsyncTask<String, Float, List<String>> {
+    //input,
+
+    public List<Integer> fetchId(String type) {
+        String jsonContent = "";
+        ArrayList<Integer> ıdList = new ArrayList<Integer>();
+        try {
+            URL url = new URL("https://hacker-news.firebaseio.com/v0/" + type + ".json?print=pretty");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line = "";
+            while (line != null) {
+                line = bufferedReader.readLine();
+                jsonContent += line;
+            }
 
 
+            JSONArray jsonArray = new JSONArray(jsonContent);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                ıdList.add(i, jsonArray.getInt(i));
 
-    @Override
-    public Void doInBackground(Void ...voids)
-    {
-        return null;
+            }
+        } catch (MalformedURLException malformedUrlException) {
+            malformedUrlException.printStackTrace();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } catch (JSONException jsonException) {
+            jsonException.printStackTrace();
+        }
+        return ıdList;
 
     }
 
-    @Override
-    protected void onPostExecute(Void aVoid) {
-        super.onPostExecute(aVoid);
+    public String fetchItem(int itemId) {
+
+        String jsonContent = "";
+        try {
+            URL url = new URL("https://hacker-news.firebaseio.com/v0/item/"+itemId+".json?print=pretty");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line = "";
+            while (line != null) {
+                line = bufferedReader.readLine();
+                jsonContent += line;
+            }
+
+            return new JSONObject(jsonContent).getString("title");
+
+        } catch (MalformedURLException malformedUrlException) {
+            malformedUrlException.printStackTrace();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } catch (JSONException jsonException) {
+            jsonException.printStackTrace();
+        }
+        return "$ error";
     }
 
+
+    @Override
+    public List<String> doInBackground(String... str) {
+
+        List<String> data = new ArrayList<>();
+        int counter=5;
+        for (int id : fetchId(str[0])) {
+            Log.d(getClass().getSimpleName(), "doInBackground: "+id);
+            data.add(fetchItem(id));
+            counter--;
+            if (counter<0)
+                break;
+        }
+        return data;
+
+    }
+
+
+    @Override
+    protected void onProgressUpdate(Float... values) {
+        super.onProgressUpdate(values);
+    }
 }
--- app/src/main/java/com/example/hackernews_api/MainActivity.java
+++ app/src/main/java/com/example/hackernews_api/MainActivity.java
@@ -2,25 +2,45 @@ package com.example.hackernews_api;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     Button click;
-    TextView data;
+    ListView data;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         click = (Button) findViewById(R.id.button);
-        data = (TextView) findViewById(R.id.fetchedData);
+        data = (ListView) findViewById(R.id.dataViewId);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, R.layout.item_layout);
+        data.setAdapter(adapter);
 
 
         click.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
+                FetchData process = new FetchData();
+                try {
+                    List<String> dataList = process.execute("topstories").get();
+                    for (String x : dataList) {
+                        adapter.add(x);
+                        Log.d("asd", x);
+                    }
+                } catch (Exception e) {
+
+                }
 
             }
         });
